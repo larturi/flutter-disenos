462d9e1410a7dd46ac85ffb8fb9ea776c9a2bfcf
larturi <lea.arturi@gmail.com>
2020-08-12 13:08:16 -0300

    Finalizada

--- lib/src/pages/botones_page.dart
+++ lib/src/pages/botones_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'dart:math';
+import 'dart:ui';
 
 class BotonesPage extends StatelessWidget {
 
@@ -13,7 +14,8 @@ class BotonesPage extends StatelessWidget {
           SingleChildScrollView(
             child: Column(
               children: [
-                _titulos()
+                _titulos(),
+                _botonera()
               ]
             )
           )
@@ -118,6 +120,69 @@ class BotonesPage extends StatelessWidget {
 
   }
 
+  Widget _botonera() {
+
+    return Table(
+      children: [
+        TableRow(
+          children: [
+            _crearBotonRedondeado(Colors.blue, Icons.border_all, 'General'),
+            _crearBotonRedondeado(Colors.purpleAccent, Icons.directions_bus, 'Bus')
+          ]
+        ),
+        TableRow(
+          children: [
+            _crearBotonRedondeado(Colors.pinkAccent, Icons.shop, 'Buy'),
+            _crearBotonRedondeado(Colors.orange, Icons.insert_drive_file, 'File')
+          ]
+        ),
+        TableRow(
+          children: [
+            _crearBotonRedondeado(Colors.blueAccent, Icons.movie_filter, 'Entertaiment'),
+            _crearBotonRedondeado(Colors.green, Icons.cloud, 'Groceries')
+          ]
+        ),
+        TableRow(
+          children: [
+            _crearBotonRedondeado(Colors.red, Icons.collections, 'Photos'),
+            _crearBotonRedondeado(Colors.teal, Icons.help_outline, 'Info')
+          ]
+        )
+      ]
+    );
+
+  }
+
+  Widget _crearBotonRedondeado(Color color, IconData icon, String texto) {
+
+    return ClipRect(
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+        child: Container(
+          height: 180.0,
+          margin: EdgeInsets.all(15.0),
+          decoration: BoxDecoration(
+            color: Color.fromRGBO(62, 66, 107, 70.0),
+            borderRadius: BorderRadius.circular(20.0)
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              SizedBox(height: 5.0),
+              CircleAvatar(
+                backgroundColor: color,
+                radius: 35.0,
+                child: Icon(icon, color: Colors.white, size: 30.0),
+              ),
+              Text(texto, style: TextStyle(color: color)),
+              SizedBox(height: 5.0)
+            ]
+          ),
+        ),
+      ),
+    );
+
+  }
 
 
 }
